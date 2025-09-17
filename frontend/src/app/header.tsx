e8245f926ab6657058b593a80d35d6c692da33d0
franco henker <francohenker@gmail.com>
2025-09-17 11:08:34 -0300

    flicker remove in /user/profile

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -4,7 +4,7 @@ import { Car } from "lucide-react"
 import Link from "next/link"
 import ThemeToggleButton from "../components/ThemeToggle";
 import { useUserStore } from "./store/useUserStore";
-
+import user_icon from '../../public/user_icon.svg'
 export default function HeaderDefault() {
 
     const handlerLogout = () => {
@@ -22,8 +22,8 @@ export default function HeaderDefault() {
     }
 
     const { isAuthenticated, login, logout } = useUserStore();
-    const user = useUserStore((state) => state.user)
-    const initial = user?.name?.charAt(0).toUpperCase() ?? '?'
+    const user = useUserStore((state) => state.user);
+    const initial = user?.name?.charAt(0).toUpperCase() ?? '?';
 
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/0" >
@@ -34,11 +34,11 @@ export default function HeaderDefault() {
                     <Link href="/" className="text-2xl font-bold text-base-content">CarDetailing</Link>
                 </div>
 
-                <nav className="text-base-content absolute left-1/2 transform -translate-x-1/2 hidden md:flex gap-6 items-center">                    
-                    <Link href="#/servicios" prefetch className="text-lg font-medium hover:text-red-600 transition-colors">
+                <nav className="text-base-content absolute left-1/2 transform -translate-x-1/2 hidden md:flex gap-6 items-center">
+                    <Link href="/servicios" prefetch className="text-lg font-medium hover:text-red-600 transition-colors">
                         Servicios
                     </Link>
-                    <Link href="#/turno" prefetch className="text-lg font-medium hover:text-red-600 transition-colors">
+                    <Link href="/turno" prefetch className="text-lg font-medium hover:text-red-600 transition-colors">
                         Reservar Turno
                     </Link>
                     <Link href="#contacto" prefetch className="text-lg font-medium hover:text-red-600 transition-colors">
@@ -46,7 +46,7 @@ export default function HeaderDefault() {
                     </Link>
                 </nav>
 
-                <div className="ml-auto flex items-center gap-4 ">                        
+                <div className="ml-auto flex items-center gap-4 ">
                     <Button variant="outline" size="icon" className="md:hidden">
                         <span className="sr-only">Menú</span>
                         <svg
@@ -72,15 +72,16 @@ export default function HeaderDefault() {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-base-content">
-                        <li>
-                            <a href='/user/profile' className="justify-between">
-                                Perfil
-                            </a>
-                        </li>
-                        <li><a href="/user/settings">Configuración</a></li>
-                        {/* <li><a href="#" onClick={handlerLogout}>Cerrar sesión</a></li> */}
-                        <li><a href="#" onClick={isAuthenticated ? handlerLogout : handleLogin}>{isAuthenticated ? 'Cerrar sesión' : 'Iniciar sesión'}</a></li>
+                        {isAuthenticated ?
+                            <>
+                                <li><a href="/user/profile" className="justify-between">Perfil</a></li>
+                                <li><a href="/user/settings">Configuración</a></li>
+                                <li><a href="#" onClick={handlerLogout}>{'Cerrar sesión'}</a></li>
+                            </> :
+                            <li><a href="#" onClick={handleLogin}>{'Iniciar sesión'}</a></li>
+                        }
                     </ul>
+
                 </div>
             </div>
 
