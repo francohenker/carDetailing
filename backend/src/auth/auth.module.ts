5d6eb4548450377dfd3c334ba326a47847d7d3d2
franco henker <francohenker@gmail.com>
2025-09-26 18:33:13 -0300

    change jwt expiration

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' }, // 60 minutes
+      signOptions: { expiresIn: '30d' },
     }),
     TypeOrmModule.forFeature([Users]),
   ],
