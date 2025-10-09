9d486fd9c25545d65497da1dd43dca9bf85cbb13
franco henker <francohenker@gmail.com>
2025-10-08 23:43:56 -0300

    smtp gmail added & remove sendgrid

---
+++ backend/src/mail.services.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+
+@Injectable()
+export class MailService {
+  private transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+  }
+
+  async sendMail(to: string, subject: string, text: string): Promise<void> {
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject,
+      text,
+    });
+  }
+}
